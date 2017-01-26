c6e96449119e1d37f020200b774493facc08bb7b
Glen Miller <Glen.Miller@varian.com>
2017-01-26 12:28:12 -0600

    Solution for parts 1-3

--- DesignPatternsSolution/Execise.Tests/UnitTests.cs
+++ DesignPatternsSolution/Execise.Tests/UnitTests.cs
@@ -1,7 +1,7 @@
 ﻿using Exercise;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Execise.Tests
+namespace Exercise.Tests
 {
     [TestClass]
     public class UnitTests
@@ -12,18 +12,28 @@ namespace Execise.Tests
         {
             //check that when you create a Library instance second time, 
             //you get exactly the same instance as for a very first time
+            Library library1 = Library.getInstance();
+            Library library2 = Library.getInstance();
+
+            Assert.AreEqual(library1, library2);
         }
 
         //test that an book was registered successfully by checking the returned Id value is not -1
         [TestMethod]
         public void BookShouldRegister()
         {
+            Book book = new Book("Mr. Smart", "My Book", 1985, 20);
+            int result = RegistrationRepository.Register(book);
+            Assert.AreNotEqual(-1, result);
         }
 
         //test that an customer was registered successfully by checking the returned Id value is not -1
         [TestMethod]
         public void CustomerShouldRegister()
         {
+            Customer customer = new Customer("John Doe", "123 Fourth St.");
+            int result = RegistrationRepository.Register(customer);
+            Assert.AreNotEqual(-1, result);
         }
 
         //test that a book can be borrowed
@@ -32,7 +42,17 @@ namespace Execise.Tests
         {
             //create a borrowable book with available amount more than one. 
             //Run BorrowOne method of the BookBorrowable instance. Check that total amount was reduced by one.
-            
-        }       
+
+            Book myBook = new Book("Glen Miller", "My Book", 1990, 50);
+
+            int oldCount = myBook.AvailableAmount;
+
+            Borrowable bookBorrowable = new Borrowable(null, new BorrowOne(myBook));
+            bookBorrowable.BorrowOne();
+
+            int newCount = myBook.AvailableAmount;
+
+            Assert.AreEqual(oldCount - 1, newCount);
+        }
     }
 }
--- DesignPatternsSolution/Exercise/BookBorrowable.cs
+++ DesignPatternsSolution/Exercise/BookBorrowable.cs
@@ -1,7 +1,48 @@
 ﻿namespace Exercise
 {
-    public class BookBorrowable 
+    public abstract class ABorrowable
     {
-       
+        public virtual void BorrowOne() { }
+        protected BorrowOne borrowOneProcedure;
+        public ABorrowable(BorrowOne borrowOneProcedure)
+        {
+            this.borrowOneProcedure = borrowOneProcedure;
+        }
+    }
+
+    public class Borrowable : ABorrowable
+    {
+        private ABorrowable borrowableToExtend;
+
+        public Borrowable(ABorrowable borrowable, BorrowOne borrowOneProcedure)
+            : base(borrowOneProcedure)
+        {
+            borrowableToExtend = borrowable;
+        }
+
+        public override void BorrowOne()
+        {
+            if (borrowableToExtend != null)
+            {
+                borrowableToExtend.BorrowOne();
+            }
+
+            borrowOneProcedure.borrowOne();
+        }
+    }
+
+    public class BorrowOne
+    {
+        private LibObject libObject;
+
+        public BorrowOne(LibObject libObject)
+        {
+            this.libObject = libObject;
+        }
+
+        public void borrowOne()
+        {
+            this.libObject.AvailableAmount--;
+        }
     }
 }
