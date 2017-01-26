b12b72a2ce2c46db47a6d67af591d2b8a6282af1
itsyomama <jeff.sarna@varian.com>
2017-01-26 16:21:56 -0600

    Jeff- Design Patterns

--- DesignPatternsSolution/Execise.Tests/UnitTests.cs
+++ DesignPatternsSolution/Execise.Tests/UnitTests.cs
@@ -12,18 +12,29 @@ namespace Execise.Tests
         {
             //check that when you create a Library instance second time, 
             //you get exactly the same instance as for a very first time
+            var lib1 = Library.getInstance();
+            var lib2 = Library.getInstance();
+
+            Assert.AreEqual(lib1, lib2);
+
         }
 
         //test that an book was registered successfully by checking the returned Id value is not -1
         [TestMethod]
         public void BookShouldRegister()
         {
+            var lib = Library.getInstance();
+            var book = new Book("one","wolf",1999,1);
+            Assert.AreNotEqual(-1,lib.Register(book));
         }
 
         //test that an customer was registered successfully by checking the returned Id value is not -1
         [TestMethod]
         public void CustomerShouldRegister()
         {
+            var lib = Library.getInstance();
+            var cust = new Customer("Bad Man", "123 Street");
+            Assert.AreNotEqual(-1, lib.Register(cust));
         }
 
         //test that a book can be borrowed
--- DesignPatternsSolution/Exercise/Library.cs
+++ DesignPatternsSolution/Exercise/Library.cs
@@ -3,16 +3,22 @@
     public class Library
     {
         //implement Singleton to make sure only one library will exist
+        static Library instance = new Library();
 
+        private Library() { }
+
+        public static Library getInstance() { return instance; }
+      //  private IRegistarable _registerObject;
         
 
-        //Implement Register method by utilizing RegistrationRepository (complete missing parts)
-        public int Register()
+        //Implement Register method by utilizing RegistrationRepository ()
+        public int Register(IRegistarable _registerObject)
         {
-            // return RegistrationRepository.Register();
+            
+            return RegistrationRepository.Register(_registerObject);
 
             //dummy, just to compile
-            return 0;
+           //return 0;
         }
     }
 }
