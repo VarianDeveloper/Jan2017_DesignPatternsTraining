db7f94cd1efe3b47e07b37bc5b0fe48afd5f2551
Rod DeWit <Rod.Dewit@varian.com>
2017-01-26 13:08:30 -0600

    Add new files to commit

---
+++ DesignPatternsSolution/Exercise/LibObjectRegistrationInfoAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class LibObjectRegistrationInfoAPI : IRegistrationInfoAPI
+    {
+        public LibObjectRegistrationInfoAPI() { }
+
+        public RegisteredObject GetRegistrationInfo(LibObject objectIn)
+        {
+            RegisteredObject regObject = new RegisteredObject();
+            regObject.Info = objectIn.NameOrTitle;
+            regObject.AvailableAmount = objectIn.AvailableAmount;
+            regObject.Id = objectIn.ObjectId;
+            return regObject;
+        }
+    }
+}
