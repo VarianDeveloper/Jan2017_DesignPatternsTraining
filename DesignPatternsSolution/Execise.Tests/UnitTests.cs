3f1751a68680d9ab8352215c5486aad69b3eadcb
cwquick <collin.quick@varian.com>
2017-01-26 11:46:31 -0600

    Task 1:  Singleton implementation Task 2:  Bridge implementation Task 3:  Decorator implementation (partial)

--- DesignPatternsSolution/Execise.Tests/UnitTests.cs
+++ DesignPatternsSolution/Execise.Tests/UnitTests.cs
@@ -12,18 +12,23 @@ namespace Execise.Tests
         {
             //check that when you create a Library instance second time, 
             //you get exactly the same instance as for a very first time
+            Assert.AreSame(Library.getInstance(), Library.getInstance(), "Instances are not the same");
         }
 
         //test that an book was registered successfully by checking the returned Id value is not -1
         [TestMethod]
         public void BookShouldRegister()
         {
+            Book book = new Book("Arthur Author", "Training in Design Patterns", 2017, 20);
+            Assert.AreNotEqual(Library.getInstance().Register(new BookRegistration(book)), -1);
         }
 
         //test that an customer was registered successfully by checking the returned Id value is not -1
         [TestMethod]
         public void CustomerShouldRegister()
         {
+            Customer cust = new Customer("Curtis Customer", "123 Real Street");
+            Assert.AreNotEqual(Library.getInstance().Register(new PersonRegistration(cust)), -1);
         }
 
         //test that a book can be borrowed
@@ -32,7 +37,12 @@ namespace Execise.Tests
         {
             //create a borrowable book with available amount more than one. 
             //Run BorrowOne method of the BookBorrowable instance. Check that total amount was reduced by one.
-            
+            Book book = new Book("Arthur Author", "Training in Design Patterns", 2017, 20);
+            int stock = 5;
+            BookBorrowable borrowableBook = new BookBorrowable(book, stock);
+
+            borrowableBook.BorrowOne();
+            Assert.AreNotEqual(stock, borrowableBook.stock);
         }       
     }
 }
