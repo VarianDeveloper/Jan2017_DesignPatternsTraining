9be1f4a6b800e3bdd1b353bfdb1f51c7397beadd
kderrett <kyle.derrett@varian.com>
2017-01-27 14:50:36 -0600

    Initial Commit

--- DesignPatternsSolution/Exercise/BookBorrowable.cs
+++ DesignPatternsSolution/Exercise/BookBorrowable.cs
@@ -1,7 +1,38 @@
 ﻿namespace Exercise
 {
-    public class BookBorrowable 
+    public abstract class ABookBorrowable
     {
-       
+        public virtual void BorrowOne() { }
+        protected BorrowOne BorrowOneProcedure;
+
+        public ABookBorrowable(BorrowOne borrowOneProcedure)
+        {
+            BorrowOneProcedure = borrowOneProcedure;
+        }
+    }
+
+    public class BookBorrowable : ABookBorrowable
+    {
+        private ABookBorrowable _bookBorrowable;
+
+        public BookBorrowable(ABookBorrowable bookBorrowable, BorrowOne borrowOne) : base(borrowOne)
+        {
+            _bookBorrowable = bookBorrowable;
+        }
+
+        public override void BorrowOne()
+        {
+            _bookBorrowable.BorrowOne();
+        }
+    }
+
+    public class BorrowOne
+    {
+        private LibObject item;
+
+        public BorrowOne(LibObject item)
+        {
+            item.AvailableAmount--;
+        }
     }
 }
