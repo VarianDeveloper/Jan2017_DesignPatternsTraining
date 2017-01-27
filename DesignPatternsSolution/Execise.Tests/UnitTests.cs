87361e2613f0fcd6879a49df31fae75dc5c4b05b
TLambert18 <Troy.Lambert@varian.com>
2017-01-27 15:44:40 -0600

    Training Solutions (Part 1 & 2 complete)

--- DesignPatternsSolution/Execise.Tests/UnitTests.cs
+++ DesignPatternsSolution/Execise.Tests/UnitTests.cs
@@ -12,18 +12,30 @@ namespace Execise.Tests
         {
             //check that when you create a Library instance second time, 
             //you get exactly the same instance as for a very first time
+            var L1 = Library.getInstance();
+            var L2 = Library.getInstance();
+
+            Assert.AreEqual(L1, L2);
         }
 
         //test that an book was registered successfully by checking the returned Id value is not -1
         [TestMethod]
         public void BookShouldRegister()
         {
+            Book B1 = new Book("Hello", "World", 2017, 10);
+            int result = RegistrationRepository.Register(B1);
+
+            Assert.AreNotEqual(-1, result);
         }
 
         //test that an customer was registered successfully by checking the returned Id value is not -1
         [TestMethod]
         public void CustomerShouldRegister()
         {
+            Customer C1 = new Customer("Troy", "Winnipeg");
+            int result = RegistrationRepository.Register(C1);
+
+            Assert.AreNotEqual(-1, result);
         }
 
         //test that a book can be borrowed
