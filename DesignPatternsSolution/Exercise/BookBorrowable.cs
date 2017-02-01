8030b0c4f0b1e77b4e6b978798f0485cb3904fe1
jason-wood-varian <jason.wood@varian.com>
2017-02-01 11:19:45 -0600

    Design patterns training exercises

--- DesignPatternsSolution/Exercise/BookBorrowable.cs
+++ DesignPatternsSolution/Exercise/BookBorrowable.cs
@@ -1,7 +1,24 @@
 ﻿namespace Exercise
 {
-    public class BookBorrowable 
+    public class BookBorrowable : Book
     {
-       
+        public Book _book { get; private set; }
+
+        public BookBorrowable(Book book, string author, string title, int year, int amount) : base(author, title, year, amount)
+        {
+            _book = book;
+        }
+
+        public void BorrowOne()
+        {
+            if (_book.AvailableAmount > 0)
+            {
+                _book.AvailableAmount--;
+            }
+            else
+            {
+                _book.AvailableAmount = -1;
+            }
+        }
     }
 }
--- DesignPatternsSolution/Exercise/Library.cs
+++ DesignPatternsSolution/Exercise/Library.cs
@@ -3,8 +3,14 @@
     public class Library
     {
         //implement Singleton to make sure only one library will exist
+        static Library libraryInstance = new Library();
 
-        
+        private Library() { }
+
+        public static Library getInstance()
+        {
+            return libraryInstance;
+        }
 
         //Implement Register method by utilizing RegistrationRepository (complete missing parts)
         public int Register()
