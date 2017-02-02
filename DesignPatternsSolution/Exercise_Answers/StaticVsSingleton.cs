22a2c193a90f0cc19aff4856255efa064442b326
AlexBeyd <beyderman.alex@gmail.com>
2017-02-02 15:06:13 -0600

    prepare for follow up

---
+++ DesignPatternsSolution/Exercise_Answers/StaticVsSingleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Answers
+{
+
+    public interface ISomeInterface
+    {
+        
+    }
+
+    public static class StaticVsSingleton
+    {
+    }
+}
